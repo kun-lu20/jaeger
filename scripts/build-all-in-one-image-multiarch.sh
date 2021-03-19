8162d26631e06db9f9d3c032666c4179b1bffcdd
Kun-Lu <kun.lu@ibm.com>
2021-03-19 16:52:20 -0400

    update

--- scripts/build-all-in-one-image-multiarch.sh
+++ scripts/build-all-in-one-image-multiarch.sh
@@ -6,8 +6,8 @@ make create-baseimg
 repo=kunluibm/jaegertest
 
 docker buildx build --push --progress=plain \
-    --build-arg base_image=localhost/baseimg:1.0.0-alpine-3.12 \
-    --build-arg debug_image=localhost/baseimg:1.0.0-alpine-3.12 \
+    --build-arg base_image=localhost:5000/baseimg:1.0.0-alpine-3.12 \
+    --build-arg debug_image=localhost:5000/baseimg:1.0.0-alpine-3.12 \
     --platform=linux/arm64,linux/amd64,linux/s390x \
     --file cmd/all-in-one/Dockerfile \
     --tag $repo/all-in-one:latest cmd/all-in-one
