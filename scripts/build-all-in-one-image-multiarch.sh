dd0cc85965b55f0b8085acb2fc120685523e73e9
Kun-Lu <kun.lu@ibm.com>
2021-03-19 16:46:10 -0400

    update

--- scripts/build-all-in-one-image-multiarch.sh
+++ scripts/build-all-in-one-image-multiarch.sh
@@ -7,7 +7,7 @@ repo=kunluibm/jaegertest
 
 docker buildx build --push --progress=plain \
     --build-arg base_image=localhost/baseimg:1.0.0-alpine-3.12 \
-    --build-arg debug_image=localhost/debugimg:1.0.0-golang-1.15-alpine \
+    --build-arg debug_image=localhost/baseimg:1.0.0-alpine-3.12 \
     --platform=linux/arm64,linux/amd64,linux/s390x \
     --file cmd/all-in-one/Dockerfile \
     --tag $repo/all-in-one:latest cmd/all-in-one
