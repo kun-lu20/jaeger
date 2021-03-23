30e47875745d5590e8cfb44528ecaa2379f12249
Kun-Lu <kun.lu@ibm.com>
2021-03-23 10:33:51 -0400

    update

---
+++ scripts/build-or-upload-other-images.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euxf -o pipefail
+
+DOCKERHUB_USERNAME=${DOCKERHUB_USERNAME:-"jaegertracingbot"}
+DOCKERHUB_TOKEN=${DOCKERHUB_TOKEN:-}
+QUAY_USERNAME=${QUAY_USERNAME:-"jaegertracing+github_workflows"}
+QUAY_TOKEN=${QUAY_TOKEN:-}
+
+# Only push images to dockerhub/quay.io for master branch or for release tags vM.N.P
+if [[ "$BRANCH" == "master" || $BRANCH =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "build multiarch images and upload to dockerhub/quay.io, BRANCH=$BRANCH"
+  PUSHTAG="type=image, push=true" make docker-images-cassandra-multiarch-nopush
+else
+  echo 'skip multiarch docker images upload, only allowed for tagged releases or master (latest tag)'
+  PUSHTAG="type=image, push=false" make docker-images-cassandra-multiarch-nopush
+fi
+
